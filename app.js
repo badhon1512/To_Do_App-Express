1f740e34c189b7d447dabd10a21e0ddecafe492e
badhon1512 <badhon1512@gmail.com>
2021-03-30 20:57:46 +0600

    ejs engine added

--- app.js
+++ app.js
@@ -4,22 +4,27 @@ const app=express();
 
 
 
+app.set('view engine','ejs');
+
+
+
 app.listen(3000);
 
 
 
 app.get('/',(req,res)=>{
 
-res.sendFile('./view/index.html',{root:__dirname});
+res.render('index')
 })
 
 
 app.get('/add-item',(req,res)=>{
 
-  res.sendFile('./view/add-item.html',{root:__dirname});
+  res.render('add-item');
 })
 
 app.use((req,res)=>{
-  res.sendFile('./view/error.html',{root:__dirname});
+
+  res.render('error');
 
 })
